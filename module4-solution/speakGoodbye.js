38fb987a52e450a08a24fbf0a2903c0038c56a7c
Kemoh <bayohumaru66@gmail.com>
2020-11-20 07:46:08 +0000

    changes

--- module4-solution/speakGoodbye.js
+++ module4-solution/speakGoodbye.js
@@ -16,12 +16,12 @@
 
 
 // DO NOT attach the speakWord variable to the 'byeSpeaker' object.
-      var speakWord = "Good Bye";
+      var speakWord = "Goodbye";
 
 // STEP 8: Rewrite the 'speak' function such that it is attached to the
 // byeSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
-      byeSpeaker.sayGoodBye = function speak(names) {
+      byeSpeaker.sayGoodbye = function speak(names) {
       console.log(speakWord + " " + byeSpeaker.names);
 }
 
