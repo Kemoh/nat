8f42b8cabb0ee278cbb935a296ff9250a68abdb7
Kemoh <bayohumaru66@gmail.com>
2020-11-20 07:58:08 +0000

    changes

--- module4-solution/speakGoodbye.js
+++ module4-solution/speakGoodbye.js
@@ -5,7 +5,7 @@
 // See Lecture 52, part 2
 
 (function (window) {
-	var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+	var names = ["John", "Jen", "Jason","Jim"];
 }) (window);
 
 // STEP 7: Create an object, called 'byeSpeaker' to which you will attach
--- module4-solution/speakHello.js
+++ module4-solution/speakHello.js
@@ -2,7 +2,7 @@
 // See Lecture 52, part 2
 
 (function (window) {
-	var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+	var names = ["Yaakov", "Paul", "Frank", "Larry", "Paula", "Laura"];
 }) (window);
 // STEP 3: Create an object, called 'helloSpeaker' to which you will attach
 // the "speak" method and which you will expose to the global context
